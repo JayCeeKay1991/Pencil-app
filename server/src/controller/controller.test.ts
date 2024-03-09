d395897e81192d63f5368d9bb5da51977e7d60f5
Joe Copplestone <strasseneck@gmail.com>
2024-03-09 11:10:18 +0100

    Feat: Add test for artist get all controller

--- server/src/controller/controller.test.ts
+++ server/src/controller/controller.test.ts
@@ -3,7 +3,6 @@ import myRouter from '../router';
 import supertest from 'supertest';
 import mongoose from 'mongoose';
 import { Artist, Project } from '../models/events'
-import { doesNotMatch } from 'assert';
 
 
 // mocks
@@ -27,7 +26,6 @@ const testProject = {
     thumbImage: 'secondtestimageurl',
     artists: []
 };
-
 
  const testArtist = {
     name: "testArtist",
@@ -59,8 +57,39 @@ const testProject = {
       }
     ]
   };
-  
 
+  const secondTestArtist = {
+    name: "secondTestArtist",
+    location: "secondTestLocation",
+    rate: "secondTestRate",
+    skills: [
+      "secondTestSkill1",
+      "secondTestSkill2",
+      "secondTestSkill3"
+    ],
+    mainSkill: "secondTestSkill1",
+    profileImg: "https://images.unsplash.com/photo-1615572359976-1fe39507ed7b?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    work: [
+      {
+        description: [
+          "secondTestDescription"
+        ],
+        images: [
+          "https://i.pinimg.com/564x/53/0a/a4/530aa4425662d0fd641dec9f3a1dfc21.jpg"
+        ]
+      },
+      {
+        description: [
+          "secondTestDescription2"
+        ],
+        images: [
+          "https://images.pexels.com/photos/12939554/pexels-photo-12939554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        ]
+      }
+    ]
+};
+
+  
 describe ('Controller', () => {
     const app  = express();
     app.use(express.json());
@@ -106,15 +135,28 @@ describe ('Controller', () => {
         await request.post('/projects')
         .send(secondTestProject)
         
-
         return await Project.find({})
             .then(projects => {
                 expect(projects.length).toEqual(2);
                 expect(projects[0].description).toBe(testProject.description);
-                expect(projects[1].description).toBe(secondTestProject.description);
+                expect(projects[1].projectName).toBe(secondTestProject.projectName);
             })
     })
 
+    it('should retrieve all artists', async () => {
+        await request.post('/artists')
+        .send(testArtist)
+
+        await request.post('/artists')
+        .send(secondTestArtist)
+
+        return await Artist.find({})
+            .then(artists => {
+                expect(artists.length).toEqual(2);
+                expect(artists[0].name).toBe(testArtist.name);
+                expect(artists[1].rate).toBe(secondTestArtist.rate);
+            })
+    })
 
 })
 
