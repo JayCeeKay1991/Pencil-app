ca99d7f41de945547233e7820725171d4f3e1936
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-03-11 16:54:49 +0100

    style: more comment styling

--- client/src/components/ArtistComments/ArtistComments.tsx
+++ client/src/components/ArtistComments/ArtistComments.tsx
@@ -6,9 +6,13 @@ export const ArtistComments = (): React.JSX.Element => {
 
   return (
     <div className="commentWrap">
+      <h5 className='commentHeading' >COMMENTS</h5>
       <ol className="commentList" >
         {/* mapping through the comments and creating a list item for each one */}
-        <li className='commentItem' >individual comment</li>
+        <li className='commentItem' >Someone's comment</li>
+        <li className='commentItem' >Someone's comment with some more text just to see the line break</li>
+        <li className='commentItem myCommentItem' >My comment</li>
+        <li className='commentItem' >Someone's comment</li>
       </ol>
       <form className='commentForm' >
         <input type="text" placeholder="Comment on this pick.."></input>
